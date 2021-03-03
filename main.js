bf2ea203c1fd1dfdb855fddf0e4be99c51d8decd
CowiVM <72988033+CowiVM@users.noreply.github.com>
2021-03-03 20:41:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+nose_x = 0
+nose_y = 0
+
 function setup() {
     canvas = createCanvas(350, 300)
     canvas.center()
@@ -24,7 +27,22 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose_x = " + results[0].pose.nose.x);
-        console.log("nose_y = " + results[0].pose.nose.y);
+        console.log(" nose_x =" + results[0].pose.nose.x);
+        console.log(" nose_y =" + results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x
+        nose_y = results[0].pose.nose.y
     }
+}
+
+function draw() {
+    image(video, 0, 0, 350, 300);
+/*     fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(nose_x, nose_y, 20);
+ */
+    image(img, nose_x, nose_y, 75, 50);
+}
+
+function preload() {
+    img = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png")
 }
